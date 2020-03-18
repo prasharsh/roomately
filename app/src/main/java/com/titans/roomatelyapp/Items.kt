8341e88134935f977277d13a7b7e30473c75b8fa
Tapan Prajapati <Tapan.Prajapati@dal.ca>
2020-03-17 21:53:16 -0300

    RecyclerView with custom layout and adapters

--- app/src/main/java/com/titans/roomatelyapp/Items.kt
+++ app/src/main/java/com/titans/roomatelyapp/Items.kt
@@ -2,9 +2,15 @@ package com.titans.roomatelyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.view.animation.LayoutAnimationController
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.titans.roomatelyapp.DataModels.Category
+import com.titans.roomatelyapp.DataModels.Item
+import com.titans.roomatelyapp.RecyclerViewAdapters.CategoryListAdapter
+import kotlinx.android.synthetic.main.activity_items.*
 
 class Items : AppCompatActivity()
 {
@@ -19,5 +25,32 @@ class Items : AppCompatActivity()
 
         backButton.setOnClickListener { v -> onBackPressed() }
         txtToolbarLabel.text = "Group Name"+" > Items"
+
+        var categories : List<Category> = getList()
+
+        var adapter = CategoryListAdapter(this@Items,categories)
+
+        categoryList.adapter = adapter
+        categoryList.layoutManager = LinearLayoutManager(this@Items)
+
+        var animation = AnimationUtils.loadLayoutAnimation(this@Items,R.anim.layout_animation_fall_down)
+        categoryList.layoutAnimation = animation
+        categoryList.scheduleLayoutAnimation()
+    }
+
+    fun getList():ArrayList<Category>
+    {
+        var list = ArrayList<Category>()
+        for(i in 1..10)
+        {
+            var items = ArrayList<Item>()
+            for(j in 1..10)
+            {
+                items.add(Item("Item$j",i%2==0))
+            }
+
+            list.add(Category("Category$i",items))
+        }
+        return list
     }
 }
