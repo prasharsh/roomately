ef997ad7cf045a82e30fd1efb0d94e8d80413d11
prashantk <prashant.kumar@dal.ca>
2020-03-19 15:14:44 -0300

    add view edit items - Prashant

--- app/src/main/java/com/titans/roomatelyapp/Items.kt
+++ app/src/main/java/com/titans/roomatelyapp/Items.kt
@@ -1,10 +1,15 @@
 package com.titans.roomatelyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.titans.roomatelyapp.items.ItemsActivity
+import com.titans.roomatelyapp.login.RegistrationActivity
 
 class Items : AppCompatActivity()
 {
@@ -16,8 +21,11 @@ class Items : AppCompatActivity()
 
         var backButton = findViewById<ImageButton>(R.id.backButton)
         var txtToolbarLabel = findViewById<TextView>(R.id.txtToolbarLabel)
-
-        backButton.setOnClickListener { v -> onBackPressed() }
+        val navToAddItem = findViewById<FloatingActionButton>(R.id.addItemFloatingButtoon)
+        backButton.setOnClickListener { _ -> onBackPressed() }
         txtToolbarLabel.text = "Group Name"+" > Items"
+
+        navToAddItem.setOnClickListener{v ->
+            startActivity(Intent(v.context, ItemsActivity::class.java))}
     }
 }
---
+++ app/src/main/java/com/titans/roomatelyapp/items/ItemsActivity.kt
@@ -0,0 +1,14 @@
+package com.titans.roomatelyapp.items
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.titans.roomatelyapp.R
+
+class ItemsActivity: AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_crud)
+    }
+}
