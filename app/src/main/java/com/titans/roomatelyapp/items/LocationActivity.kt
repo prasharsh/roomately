82661eec7a452246bbda05f0c46d75ebd0909fb8
prashantk <prashant.kumar@dal.ca>
2020-03-30 16:51:36 -0300

    location module

---
+++ app/src/main/java/com/titans/roomatelyapp/items/LocationActivity.kt
@@ -0,0 +1,48 @@
+package com.titans.roomatelyapp.items
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import com.titans.roomatelyapp.R
+import java.util.*
+
+
+class LocationActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.item_location)
+        Places.initialize(applicationContext, getString(R.string.places_api))
+
+        val autocompleteFragment =
+            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment?
+
+        // Specify the types of place data to return.
+        // Specify the types of place data to return.
+        autocompleteFragment!!.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS))
+
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                val intent = Intent(this@LocationActivity, ItemsActivity::class.java)
+                intent.putExtra("Name", place.name )
+                intent.putExtra("Address", place.address)
+
+                startActivity(intent)
+                }
+
+            override fun onError(status: Status) {
+                Toast.makeText(applicationContext,"An error occurred: $status", Toast.LENGTH_LONG).show();
+            }
+        })
+    }
+}
